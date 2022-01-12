5d1af85b7ce9be5d2599319060f2b148153059e9
fajjarnr <zfajart@gmail.com>
2022-01-12 09:57:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Responsive Furniture Website
+
+### Responsive Furniture Website
+
+- Responsive Furniture Website Using HTML, CSS and Bootstrap.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+![furniture website](/preview.png)
---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.mySwiperPopular', {
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.bxs-chevron-right-circle',
+    prevEl: '.bxs-chevron-left-circle',
+  },
+});
+
+var swiperBlog = new Swiper('.mySwiperBlog', {
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+});
